9556a3de26e4dcbf7c130fa25e982afff8c0298f
Ni55aN <huccah@gmail.com>
2022-10-17 21:10:29 +0300

    fix eslint extensions match

--- src/lint/linter.ts
+++ src/lint/linter.ts
@@ -4,5 +4,5 @@ import { join } from 'path'
 export async function lint(fix?: boolean) {
   const path = join(process.cwd(), '.eslintrc')
 
-  await execa('eslint', ['-c', path, join(process.cwd(), 'src', '**'), '--ext', '.ts', ...(fix ? ['--fix'] : [])], { stdio: 'inherit' })
+  await execa('eslint', ['-c', path, join(process.cwd(), 'src'), '--ext', '.ts', ...(fix ? ['--fix'] : [])], { stdio: 'inherit' })
 }
