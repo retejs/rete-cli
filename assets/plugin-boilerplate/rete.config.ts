aa044ae16662af4752f4e280ebb9df51268d3dc4
Ni55aN <huccah@gmail.com>
2023-01-28 21:21:09 +0200

    add rete dependency to plugin boilerplate

--- assets/plugin-boilerplate/rete.config.ts
+++ assets/plugin-boilerplate/rete.config.ts
@@ -2,5 +2,8 @@ import { ReteOptions } from 'rete-cli'
 
 export default <ReteOptions>{
     input: 'src/index.ts',
-    name: '{{namespace}}'
+    name: '{{namespace}}',
+    globals: {
+        'rete': 'Rete'
+    }
 }
