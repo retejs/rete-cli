86b87e2cd4e10b611d631257d00a37c539176b07
Ni55aN <stoliarov.v.s@gmail.com>
2024-04-20 15:19:19 +0300

    fix(test): coverage all files in src

--- src/test/index.ts
+++ src/test/index.ts
@@ -6,6 +6,7 @@ export default async function (watch?: boolean) {
       '--preset', 'ts-jest',
       '--coverage',
       '--testEnvironment', 'node',
+      '--collectCoverageFrom', 'src/**/*',
       '--testMatch', '**/*.test.ts',
       ...(watch ? ['--watch'] : [])
     ], { stdio: 'inherit' })
