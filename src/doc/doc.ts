28da5e00daa891cf285ea4d098dc8d15f706871e
Ni55aN <stoliarov.v.s@gmail.com>
2024-08-28 19:10:12 +0300

    feat: update typedoc

--- src/doc/doc.ts
+++ src/doc/doc.ts
@@ -1,26 +1,27 @@
 import { join } from 'path'
-import { Application, TSConfigReader, TypeDocReader } from 'typedoc'
+import { Application, OptionDefaults, TSConfigReader, TypeDocReader } from 'typedoc'
 
 import { SOURCE_FOLDER } from '../consts'
 
 export async function doc(entries?: string[]) {
   const root = process.cwd()
   const outputDir = join(root, 'docs')
-  const app = new Application()
-
-  app.options.addReader(new TSConfigReader())
-  app.options.addReader(new TypeDocReader())
-
   const entryPoints = (entries || [join(SOURCE_FOLDER, 'index.ts')]).map(entry => join(root, entry))
-
-  app.bootstrap({
+  const app = await Application.bootstrap({
     excludeNotDocumented: true,
     excludePrivate: true,
     excludeExternals: true,
+    blockTags: [
+      ...OptionDefaults.blockTags,
+      '@priority',
+      '@emits',
+      '@listens',
+      '@constructor'
+    ],
     entryPoints
-  })
+  }, [new TSConfigReader(), new TypeDocReader()])
 
-  const project = app.convert()
+  const project = await app.convert()
 
   if (!project) throw new Error('Failed to generate docs')
 
